d6c56be6b879f9f3d7d6d575b860bc2280036fdf
VitoS <ikn0@proton.me>
2024-01-22 14:16:49 +0100

    .

---
+++ README.md
@@ -0,0 +1,23 @@
+bing.com: [mySmartUSB MK2](https://www.bing.com/search?q=mySmartUSB+MK2)
+Gerne helfe ich dir bei der Erstellung eines Informationsblatts für den **mySmartUSB MK2**. Dieses Gerät ist ein USB-Programmer und Kommunikationsinterface für AVR-Mikrocontroller¹. Hier sind einige wichtige Informationen, die dir bei der Verwendung des mySmartUSB MK2 helfen können:
+
+- Der mySmartUSB MK2 ist ein ISP-Programmer mit USB-Anschluss und integrierter Kommunikationsschnittstelle¹.
+- Das Board kann zur Programmierung von AVR-Mikrocontrollern im Zielsystem verwendet werden¹.
+- Fuse- und Lock-Bits können gesetzt werden¹.
+- Eine serielle Datenübertragung zwischen Mikrocontroller und Computer kann hergestellt werden¹.
+- Die Kommunikation per UART, TWI, I²C ist möglich¹.
+- Der mySmartUSB MK2 ist sehr einfach und sicher mit ISP-Schnittstelle zu handhaben¹.
+- Der Controller muss zur Programmierung nicht aus der Zielumgebung genommen werden¹.
+- Es ist keine externe Spannungsversorgung durch ein Netzteil oder Batterie notwendig¹.
+- Der mySmartUSB MK2 ist kompatibel mit verschiedenen 8-Bit RISC-Prozessoren mit ISP-Schnittstelle der AVR-Familie¹.
+- Der mySmartUSB MK2 ist umschaltbar zwischen Programmier- und Kommunikationsmodus¹.
+- Die Firmware kann nach AVR AN910/AN911 sowie STK500 Programmierstandard aktualisiert werden¹.
+
+Ich hoffe, dass diese Informationen dir bei der Erstellung deines Informationsblatts helfen. Wenn du weitere Fragen hast, stehe ich dir gerne zur Verfügung.
+
+¹: [myAVR Mikrocontroller-Shop](https://shop.myavr.de/index.php?sp=article.sp.php&artID=200179)
+
+Quelle: Unterhaltung mit Bing, 22.1.2024
+(1) myAVR Mikrocontroller-Shop - mySmartUSB MK2. https://shop.myavr.de/index.php?sp=article.sp.php&artID=200179.
+(2) myAVR Technische Beschreibung technical description - SiSy. http://sisy.name/mymcu_download/produkte/mysmartusb_mk2/techb_mysmartusb-mk2_de_en.pdf.
+(3) Anwendungsbeispiel 3-5V_mySmartUSB-MK2_2008-11 - SiSy. https://sisy.name/mymcu_download/produkte/mysmartusb_mk2/bsp_mysmartusb-mk2-volt-switch_de_en.pdf.
---
+++ senden-empfangen.asm
@@ -0,0 +1,63 @@
+;+----------------------------------------------------------------------
+;| Title		: myAVR Grundgerüst für ATmega8
+;+----------------------------------------------------------------------
+;| Funktion		: ...
+;| Schaltung	: ...
+;+----------------------------------------------------------------------
+;| Prozessor	: ATmega8
+;| Takt		: 3,6864 MHz
+;| Sprache       	: Assembler
+;| Datum         	: ...
+;| Version       	: ...
+;| Autor         	: ...
+;+----------------------------------------------------------------------
+.include	"AVR.H"
+;------------------------------------------------------------------------
+;Reset and Interrupt vector             ;VNr.  Beschreibung
+	rjmp	main	;1   POWER ON RESET
+	reti		;2   Int0-Interrupt
+	reti		;3   Int1-Interrupt
+	reti		;4   TC2 Compare Match
+	reti		;5   TC2 Overflow
+	reti		;6   TC1 Capture
+	reti		;7   TC1 Compare Match A
+	reti		;8   TC1 Compare Match B
+	reti		;9   TC1 Overflow
+	reti		;10  TC0 Overflow
+	reti		;11  SPI, STC Serial Transfer Complete
+	reti		;12  UART Rx Complete
+	reti		;13  UART Data Register Empty
+	reti		;14  UART Tx Complete
+	reti		;15  ADC Conversion Complete
+	reti		;16  EEPROM Ready
+	reti		;17  Analog Comparator
+	reti		;18  TWI (I²C) Serial Interface
+	reti		;19  Store Program Memory Ready
+;------------------------------------------------------------------------
+;Start, Power ON, Reset
+main:	ldi	r16,lo8(RAMEND)
+	out	ioSPL,r16
+	ldi	r16,hi8(RAMEND)
+	out	ioSPH,r16
+	;Hier Init-Code eintragen.
+	
+	ldi	r16, 23	;konstante 23 in R16 schreiben
+	out	UBRRL, r16	; Wert aus 16 in ubrr schreiben
+	
+	sbi	UCSRB, 3	;UART Senden Aktivieren bit 3 in UCSRB setzen
+;------------------------------------------------------------------------
+mainloop:	wdr
+	;Hier den Quellcode eintragen.
+	
+	ldi	r16, 'H'
+	rcall	senden
+	
+ende:	rjmp ende
+;------------------------------------------------------------------------
+
+senden:	sbis	UCSRA, 5	;Überspringe nächtsten befehl
+	rjmp	senden	;wenn bit 5 in ucsra wert '1' hat springe zum senden
+	
+	out	UDR, r16
+	ret
+	
